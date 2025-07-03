49f2c0e04a8a4b654c99e09d1aab5b45831908a1
MukhtarH10 <mukhtarhamzat2@gmail.com>
2025-07-03 07:48:56 -0400

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -2,7 +2,5 @@ export default {
   name: 'balance',
   run: async ({ message, userData }) => {
     message.reply(`Wallet: 💰 ${userData.coins} coins.`);
-    console.log(`${message.author.username}'s wallet: ${userData.coins}`);
-    node index.js
   }
 };
